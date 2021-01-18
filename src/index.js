03da500673a1818d3bbcc086c176cb846e0d0f31
dev_hp_eric <dev_hp_eric@iseeg.com>
2021-01-17 22:08:41 -0500

    --

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 //assing
 const express = require('express');
-const app = express(); 
+const app = express();
 const path = require('path');
 const indexRoute = require('./routes');
 const anime = require('animejs');
 
 //settings
 app.set('port', 8000);
-app.set('views', path.join(__dirname,'views'));
-app.engine('html',require('ejs').renderFile);
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 //middlewares
 
@@ -17,7 +17,7 @@ app.set('view engine','ejs');
 app.use(indexRoute);
 
 //static files
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //listening the server
 app.listen(app.get('port'), () => {
